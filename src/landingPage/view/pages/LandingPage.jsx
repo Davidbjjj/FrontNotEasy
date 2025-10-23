152bc3581ce48df7ddaa4b04e30ffb4d2ce4f87a
davidbjjj <pondavid103@gmail.com>
2025-10-23 10:58:11 -0300

    header funcional

--- src/landingPage/view/pages/LandingPage.jsx
+++ src/landingPage/view/pages/LandingPage.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import HeaderView from "../../presentation/components/HeaderView.jsx";
+import BannerSection from "../../presentation/components/BannerSection.jsx";
 
-const LandingPage: React.FC = () => {
-  return <HeaderView />;
+const LandingPage = () => {
+  return (
+    <>
+      <HeaderView />
+      <BannerSection />
+    </>
+  );
 };
 
 export default LandingPage;
