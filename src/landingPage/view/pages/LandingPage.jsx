cf937e8bdc940df7de17e4969031950f455c6016
davidbjjj <pondavid103@gmail.com>
2025-10-23 19:06:33 -0300

    atualizações

--- src/landingPage/view/pages/LandingPage.jsx
+++ src/landingPage/view/pages/LandingPage.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import HeaderView from "../../presentation/components/HeaderView.jsx";
 import BannerSection from "../../presentation/components/BannerSection.jsx";
-
+import StudyPerformance from "../../presentation/components/StudyPerformance.js";
+import NotEasyDashboard from "../../presentation/components/NotEasyDashboard.jsx";
 const LandingPage = () => {
   return (
     <>
       <HeaderView />
       <BannerSection />
+      <StudyPerformance />
+      <NotEasyDashboard/>
     </>
   );
 };
